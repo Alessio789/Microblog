b53578e9b69ec51a79bc034056ec4ab6e01d9cb7
Alessio789 <alessio.trentin3@gmail.com>
2020-03-13 11:27:56 +0100

    Minor Changes

--- src/main/java/blog/SaltGenerator.java
+++ src/main/java/blog/SaltGenerator.java
@@ -6,21 +6,22 @@ import java.util.Random;
 
 public class SaltGenerator {
 
-  private static final Random RANDOM = new SecureRandom();
+    private static final Random RANDOM = new SecureRandom();
 
-  /**
-   * static utility class
-   */
-  private SaltGenerator() { }
+    /**
+     * static utility class
+     */
+    private SaltGenerator() {
+    }
 
-  /**
-   * Returns a random salt to be used to hash a password.
-   *
-   * @return a 16 bytes random salt
-   */
-  public static byte[] getNextSalt() {
-    byte[] salt = new byte[16];
-    RANDOM.nextBytes(salt);
-    return salt;
-  }
+    /**
+     * Returns a random salt to be used to hash a password.
+     *
+     * @return a 16 bytes random salt
+     */
+    public static byte[] getNextSalt() {
+        byte[] salt = new byte[16];
+        RANDOM.nextBytes(salt);
+        return salt;
+    }
 }
