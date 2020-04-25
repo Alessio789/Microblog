b15a0ac2f1d788caa59eecfe82a6aa8c7c537cdf
Alessio789 <alessio.trentin3@gmail.com>
2020-04-25 13:12:43 +0200

    Implemented Spring Security

---
+++ src/main/java/it/marconivr/microblog/security/UserPrincipal.java
@@ -0,0 +1,77 @@
+package it.marconivr.microblog.security;
+
+import java.util.ArrayList;
+
+import java.util.Collection;
+
+import it.marconivr.microblog.entities.User;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * UserPrincipal
+ */
+public class UserPrincipal implements UserDetails {
+
+    private User user;
+
+    public UserPrincipal(User user) {
+        super();
+        this.user = user;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        ArrayList<GrantedAuthority> gaList = new ArrayList<>();
+
+
+        this.user.getRoleList()
+                .forEach(p -> {
+                    GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + p);
+                    gaList.add(authority);
+                });
+
+        return gaList;
+    }
+
+    @Override
+    public String getPassword() {
+        // TODO Auto-generated method stub
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,12 @@
-
-INSERT INTO user (username, email, password, role, salt) VALUES
-('Alessio', 'alessio.trentin3@gmail.com', '03da8881bf08561c3e0d3f88bb86920956faa69ed84fe81093b6ef5592cbbc88', 'ADMIN', 'c/vZylHXCG1VardmjmLMhw=='),
-('TestUser', 'prova@example.com', '25bf926124fd43cf8a12ecf955d535781531c5617e06086ddd3305f390d9a944', 'USER', '9UDjgg8kZqoZXyiSczYq0w==');
+INSERT INTO user (username, email, password, roles) VALUES
+('Alessio',	'alessio.trentin3@gmail.com',	'$2a$10$oAa/xcO/vODrrRKjy4uIK.Bn77XgPTZKNnbxOe/EjZtBqGO3Lz8um',	'ADMIN'),
+('TestUser', 'test@user.com', '$2a$10$IBfz7SemInEeUZ77S72vPuIancOmckRiUMqBHnGTmjar5HKY9OFFC', 'USER');
 
 INSERT INTO post (id, body, date_hour, title, user_username) VALUES
-(1, 'This is a test post', '2020-03-19 15:17:08.981', 'Test', 'Alessio');
-
+(1,	'Hi, this is a test post',	'2020-04-25 12:45:12.859', 'Test', 'Alessio'),
+(2,	'This is a Post',	'2020-04-25 13:45:12.859', 'Post', 'Alessio');
 
 INSERT INTO comment (id, body, date_hour, post_id, user_username) VALUES
-('1', 'Hi, this is a test comment', '2020-03-19 18:07:14.09', '1', 'TestUser');
+(1,	'Hi, this is a test comment', '2020-04-25 13:07:00.61',	1, 'TestUser'),
+(2,	'Hi', '2020-04-25 14:07:00.61',	1, 'TestUser');
+
