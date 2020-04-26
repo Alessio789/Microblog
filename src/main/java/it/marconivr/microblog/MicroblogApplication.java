fb8cce77ffd5320a3071c2d6020bde8e94597fb3
Alessio789 <alessio.trentin3@gmail.com>
2020-04-26 12:19:24 +0200

    Deleted HTTPS, updated REST Controllers

--- src/main/java/it/marconivr/microblog/MicroblogApplication.java
+++ src/main/java/it/marconivr/microblog/MicroblogApplication.java
@@ -1,15 +1,8 @@
 package it.marconivr.microblog;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -24,35 +17,5 @@ public class MicroblogApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(MicroblogApplication.class, args);
-    }
-
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("COONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-
-        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
-
-        return tomcat;
-    }
-
-    private Connector httpToHttpsRedirectConnector() {
-
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme("http");
-        connector.setPort(8080);
-        connector.setSecure(false);
-        connector.setRedirectPort(8443);
-        return connector;
     }
 }
